3b6d213f1ccafcc2e8c28df2b3fbd6a6c8dd0d2e
Erika <erikasolsen@yahoo.com>
2022-01-16 18:21:07 -0800

    Added Comments to ItemModel.cs

--- Mine/Mine/Models/ItemModel.cs
+++ Mine/Mine/Models/ItemModel.cs
@@ -7,7 +7,7 @@ namespace Mine.Models
         public string Id { get; set; }
         public string Text { get; set; }
         public string Description { get; set; }
-     
+        // Value attribute of the item is +9 damage
         public int Value { get; set; } = 0;
     }
 }
