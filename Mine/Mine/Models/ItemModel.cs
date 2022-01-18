8123955f5311ce2f4b829fe5e68e8ea205a45e16
Erika <erikasolsen@yahoo.com>
2022-01-17 23:36:52 -0800

    Added comments to ItemModel for the item attributes

--- Mine/Mine/Models/ItemModel.cs
+++ Mine/Mine/Models/ItemModel.cs
@@ -2,10 +2,16 @@
 
 namespace Mine.Models
 {
+    /// <summary>
+    /// Items for the characters and monsters to use
+    /// </summary>
     public class ItemModel
     {
+        // ID for the item
         public string Id { get; set; }
+        // Display text for the item
         public string Text { get; set; }
+        // Description for the item
         public string Description { get; set; }
         // Value attribute of the item is +9 damage
         public int Value { get; set; } = 0;
